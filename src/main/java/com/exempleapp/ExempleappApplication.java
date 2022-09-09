48106c2781c295d0a6bcde53b824101fd1a5187a
Alex@TKA <alextchoupou@gmail.com>
2022-09-10 00:42:19 +0100

    Initial commit

---
+++ src/main/java/com/exempleapp/ExempleappApplication.java
@@ -0,0 +1,13 @@
+package com.exempleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExempleappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExempleappApplication.class, args);
+    }
+
+}
